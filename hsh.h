a371e2c553d99a57975c9398442ad5ad275d00bc
OuardaHolberton <12985@holbertonstudents.com>
2026-04-18 19:39:57 +0200

    include guard and first prototype

---
+++ hsh.h
@@ -0,0 +1,13 @@
+#ifndef HSH_H
+#define HSH_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+
+void exec_cmd(char *line, char **argv);
+
+#endif
