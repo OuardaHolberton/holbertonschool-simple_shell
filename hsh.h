d9ea2b46238854d97a9329ee5a73ff4d90bd32a3
OuardaHolberton <12985@holbertonstudents.com>
2026-04-21 16:04:00 +0200

    mise a jour du hsh.h

--- hsh.h
+++ hsh.h
@@ -6,8 +6,19 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 
+extern char **environ;
 
-void exec_cmd(char *line, char **argv);
+int main(int argc, char **argv);
+void exec_cmd(char **args, char **argv);
+char **parse_line(char *line);
+void free_args(char **args);
+char *get_path(void);
+char *check_cmd(char *dir, char *cmd);
+char *find_path(char *cmd);
+int handle_builtins(char **args, char *line);
+void handle_env(void);
+void handle_exit(char **args, char *line);
 
-#endif
+#endif 
